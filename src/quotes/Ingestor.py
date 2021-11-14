10a88c0af908c73ead1eb111effa8a8514737260
Ahmed Gharib <a.gharib89@yahoo.com>
2021-11-14 16:24:35 +0200

    Add readme

--- src/quotes/Ingestor.py
+++ src/quotes/Ingestor.py
@@ -1,3 +1,13 @@
+#!/bin/env python
+# -*- coding: utf-8 -*-
+#
+# PROGRAMMER: Ahmed Gharib
+# DATE CREATED: Sun Nov 14 2021
+# REVISED DATE: Sun Nov 14 2021
+# PURPOSE:
+#
+##
+
 from typing import List
 
 from .quote_model import QuoteModel
@@ -9,10 +19,23 @@ from .TextIngestor import TextIngestor
 
 
 class Ingestor(IngestorInterface):
+    """Check for the type of the document and parse the document with
+    the correct ingestor.
+    """
+
     Ingestors = [DocxIngestor, CSVIngestor, PDFIngestor, TextIngestor]
 
     @classmethod
-    def parse(cls, path: str) -> List[DocxIngestor]:
+    def parse(cls, path: str) -> List[QuoteModel]:
+        """Parse any kind of document that can be handeled
+        with one of the Ingestors.
+
+        Args:
+            path (str): path to the document to be ingested.
+
+        Returns:
+            List[QuoteModel]: list of QuoteModel objects.
+        """
         for ingestor in cls.Ingestors:
             if ingestor.can_ingest(path):
                 return ingestor.parse(path)
